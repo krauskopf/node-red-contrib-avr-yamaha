6c254fee540208834bccd5801a61a3fc2c37e72a
Sebastian Krauskopf <mail@sebakrau.de>
2016-03-12 17:17:31 +0100

    moved code for GET and PUT from node event to functions

--- avr-yamaha.js
+++ avr-yamaha.js
@@ -25,6 +25,8 @@ SOFTWARE.
 module.exports = function(RED) {
   var YamahaAPI = require("yamaha-nodejs");
   var xml2js = require('xml2js');
+  var deferred = require('deferred');
+
 
 	/* ---------------------------------------------------------------------------
 	 * CONFIG node
@@ -44,108 +46,87 @@ module.exports = function(RED) {
     this.subscriptions = {};
     this.inputSocket = undefined;
     this.devDesc = undefined;
-
-    // Define Helper Functions
-    var node = this;
-    var startEventListener = function() {
-      node.log("STARTING event listener");
-
-      var net = require('net');
-      var dgram = require('dgram');
-      node.inputSocket = dgram.createSocket({type:'udp4', reuseAddr: true});
-      node.inputSocket.on('message', function (msg, rinfo) {
-
-        node.log("[" + rinfo.address + "] --> " + msg.toString());
-
-        if (rinfo.address == node.address) {
-          //node.log("[" + rinfo.address + "] --> " + msg.toString());
-
-          // Split to header and body
-          msg = msg.toString().split("\r\n\r\n");
-          var header = msg[0];
-          var body = msg[1];
-
-          // Ignore UPNP search requests
-          var method = header.split("\r\n").shift().split(' ').shift().trim();
-          if (method == 'M-SEARCH'){
-          	return;
-          }
-
-          // Parse rest of header
-          var arr = header.match(/[^\r\n]+/g);
-      		var headerInfo = {};
-      		for (var i = 1; i < arr.length; ++i){
-            var tem = arr[i].split(/:(.+)?/);
-            if (typeof(tem[1]) == 'string'){ tem[1] = tem[1].trim(); }
-            headerInfo[tem[0].toLowerCase()] = tem[1];
-  		    };
-          //node.log("METHOD: " + method);
-          node.log("BODY: " + body);
-          //node.log("NTS: " + headerInfo['nts']);
-
-          // NOTIFY messages tell us about the properties that changed
-          if (method == "NOTIFY" && headerInfo['nts'] == "yamaha:propchange") {
-
-
-            var parseString = require('xml2js').parseString;
-            parseString(body, function (err, result) {
-              if (err) {
-                node.error("Failed to parse the event with error: " + err);
-                return;
-              }
-
-              for (var i in result.YAMAHA_AV.Main_Zone[0].Property) {
-                var prop = result.YAMAHA_AV.Main_Zone[0].Property[i];
-                node.log('Property-Change: ' + prop);
-
-                // Event Notification only notifies that there is a change in each item.
-                // It is necessary to issue the GET command to obtain the content of the item with the change.
-                switch (expression) {
-                  case 'Volume':
-
-                    break;
-                  default:
-
-                }
-
-                for (var s in node.subscriptions) {
-                  node.subscriptions[s].handler("NOTIFY", body);
-                }
-
-              }
-
-            });
-
-
-          }
-        }
-      });
-
-      node.inputSocket.on('listening', function () {
-        var address = node.inputSocket.address();
-        node.log('UDP client listening on ' + address.address + ":" + address.port);
-        node.inputSocket.setBroadcast(true)
-        node.inputSocket.setMulticastTTL(128);
-        node.inputSocket.addMembership('239.255.255.250', '192.168.0.101');
-        node.inputSocket.addMembership('239.255.255.250', '127.0.0.1');
-
-/*
-          var message = new Buffer(
-            "M-SEARCH * HTTP/1.1\r\n" +
-            "HOST: 239.255.255.250:1900\r\n" +
-            "MAN: \"ssdp:discover\"\r\n" +
-            "ST: "+'ssdp:all'+"\r\n" + // Essential, used by the client to specify what they want to discover, eg 'ST:ge:fridge'
-            "MX: 3\r\n" + // 1 second to respond (but they all respond immediately?)
-            "\r\n");
-
-          node.inputSocket.send(message, 0, message.length, 1900, "239.255.255.250");
-*/
-      });
-
-      node.inputSocket.bind(1900);
-    }
+    this.yamaha = new YamahaAPI(this.address);
 
     // Define functions called by nodes
+    var node = this;
+    this.sendGetCommand = function(topic) {
+      var def = deferred();
+
+      // Build command string from topic
+      var command = '<YAMAHA_AV cmd="GET">';
+      var elements = topic.split('.');
+      elements.forEach(function(element) { command += '<' + element + '>' });
+      command += 'GetParam';
+      elements.reverse().forEach(function(element) { command += '</' + element + '>' });
+      command += '</YAMAHA_AV>';
+
+      // Request the data using yamaha get
+      if (node.debug) {
+        node.log('sending GET command:' + command);
+      }
+      node.yamaha.SendXMLToReceiver(command).then(function(response) {
+        xml2js.Parser({ explicitArray: false }).parseString(response, function (err, result) {
+          if (err) {
+            def.reject("Failed to parse the response with error: " + err);
+            return;
+          }
+
+          // Remove reference node path from payload
+          var payload = result['YAMAHA_AV'];
+          elements.reverse().forEach(function(element) { payload = payload[element]; });
+
+          def.resolve(payload);
+        });
+      })
+      .catch(function(error){
+        def.reject(error);
+      });
+
+      return def.promise;
+    }
+
+    this.sendPutCommand = function(topic, payload) {
+      var def = deferred();
+
+      // Build command string from topic
+      var command = '<YAMAHA_AV cmd="PUT">';
+      var elements = topic.split('.');
+      elements.forEach(function(element) { command += '<' + element + '>' });
+      command += payload.toString().trim();
+      elements.reverse().forEach(function(element) { command += '</' + element + '>' });
+      command += '</YAMAHA_AV>';
+
+      // Write the data using yamaha put
+      if (node.debug) {
+        node.log('sending PUT command:' + command);
+      }
+      node.yamaha.SendXMLToReceiver(command).then(function(response){
+        xml2js.Parser({ explicitArray: false }).parseString(response, function (err, result) {
+          if (err) {
+            def.reject('Failed to parse the response with error: ' + err);
+            return;
+          }
+
+          // Traverse the response data and check for valid resonse.
+          var payload = result['YAMAHA_AV'];
+          elements.reverse().forEach(function(element) {
+            if (!payload.hasOwnProperty(element)) {
+              node.error('Received unexpected response data: ' + result);
+            }
+            payload = payload[element];
+          });
+
+          def.resolve(payload);
+        });
+      })
+      .catch(function(error){
+        def.reject('Failed to write data to AVR with error: ' + error);
+      });
+
+      return def.promise;
+    }
+
     this.users = {};
     this.register = function(mqttNode){
       node.users[mqttNode.id] = mqttNode;
@@ -252,6 +233,106 @@ module.exports = function(RED) {
       }
     };
 
+
+    // Internal sub routines
+    var startEventListener = function() {
+      node.log("STARTING event listener");
+
+      var net = require('net');
+      var dgram = require('dgram');
+      node.inputSocket = dgram.createSocket({type:'udp4', reuseAddr: true});
+      node.inputSocket.on('message', function (msg, rinfo) {
+
+        node.log("[" + rinfo.address + "] --> " + msg.toString());
+
+        if (rinfo.address == node.address) {
+          //node.log("[" + rinfo.address + "] --> " + msg.toString());
+
+          // Split to header and body
+          msg = msg.toString().split("\r\n\r\n");
+          var header = msg[0];
+          var body = msg[1];
+
+          // Ignore UPNP search requests
+          var method = header.split("\r\n").shift().split(' ').shift().trim();
+          if (method == 'M-SEARCH'){
+            return;
+          }
+
+          // Parse rest of header
+          var arr = header.match(/[^\r\n]+/g);
+          var headerInfo = {};
+          for (var i = 1; i < arr.length; ++i){
+            var tem = arr[i].split(/:(.+)?/);
+            if (typeof(tem[1]) == 'string'){ tem[1] = tem[1].trim(); }
+            headerInfo[tem[0].toLowerCase()] = tem[1];
+          };
+          //node.log("METHOD: " + method);
+          node.log("BODY: " + body);
+          //node.log("NTS: " + headerInfo['nts']);
+
+          // NOTIFY messages tell us about the properties that changed
+          if (method == "NOTIFY" && headerInfo['nts'] == "yamaha:propchange") {
+
+
+            var parseString = require('xml2js').parseString;
+            parseString(body, function (err, result) {
+              if (err) {
+                node.error("Failed to parse the event with error: " + err);
+                return;
+              }
+
+              for (var i in result.YAMAHA_AV.Main_Zone[0].Property) {
+                var prop = result.YAMAHA_AV.Main_Zone[0].Property[i];
+                node.log('Property-Change: ' + prop);
+
+                // Event Notification only notifies that there is a change in each item.
+                // It is necessary to issue the GET command to obtain the content of the item with the change.
+                switch (prop) {
+                  case 'Volume':
+
+                    break;
+                  default:
+
+                }
+
+                for (var s in node.subscriptions) {
+                  node.subscriptions[s].handler("NOTIFY", body);
+                }
+
+              }
+
+            });
+
+
+          }
+        }
+      });
+
+      node.inputSocket.on('listening', function () {
+        var address = node.inputSocket.address();
+        node.log('UDP client listening on ' + address.address + ":" + address.port);
+        node.inputSocket.setBroadcast(true)
+        node.inputSocket.setMulticastTTL(128);
+        node.inputSocket.addMembership('239.255.255.250', '192.168.0.101');
+        node.inputSocket.addMembership('239.255.255.250', '127.0.0.1');
+
+/*
+          var message = new Buffer(
+            "M-SEARCH * HTTP/1.1\r\n" +
+            "HOST: 239.255.255.250:1900\r\n" +
+            "MAN: \"ssdp:discover\"\r\n" +
+            "ST: "+'ssdp:all'+"\r\n" + // Essential, used by the client to specify what they want to discover, eg 'ST:ge:fridge'
+            "MX: 3\r\n" + // 1 second to respond (but they all respond immediately?)
+            "\r\n");
+
+          node.inputSocket.send(message, 0, message.length, 1900, "239.255.255.250");
+*/
+      });
+
+      node.inputSocket.bind(1900);
+    }
+
     // Define config node event listeners
     node.on("close", function(done){
       node.closing = true;
@@ -300,7 +381,7 @@ module.exports = function(RED) {
         }
       });
     } else {
-      this.error(RED._("mqtt.errors.missing-config"));
+      this.error(RED._("avr-yamaha.errors.missing-config"));
     }
   }
   RED.nodes.registerType("AVR-Yamaha-in", AvrYamahaNodeIn);
@@ -314,56 +395,37 @@ module.exports = function(RED) {
     RED.nodes.createNode(this, config);
 
 		// Save settings in local node
-		this.device = RED.nodes.getNode(config.device);
+    this.device = config.device;
+    this.deviceNode = RED.nodes.getNode(this.device);
 		this.name = config.name;
 		this.topic = config.topic;
-		this.yamaha = new YamahaAPI(this.device.address);
+
     var node = this;
+    if (this.deviceNode) {
 
-		// Input handler, called on incoming flow
-    this.on('input', function(msg) {
+  		// Input handler, called on incoming flow
+      this.on('input', function(msg) {
 
-      // If no topic is given in the config, then we us the topic in the msg.
-      var topic = (node.topic) ? node.topic : msg.topic;
-      if (!topic) {
-        node.error('No topic given. Specify either in the config or via msg.topic!');
-        return;
-      }
+        // If no topic is given in the config, then we us the topic in the msg.
+        var topic = (node.topic) ? node.topic : msg.topic;
+        if (!topic) {
+          node.error('No topic given. Specify either in the config or via msg.topic!');
+          return;
+        }
 
-      // Build command string from topic
-      var command = '<YAMAHA_AV cmd="GET">';
-      var elements = topic.split('.');
-      elements.forEach(function(element) { command += '<' + element + '>' });
-      command += 'GetParam';
-      elements.reverse().forEach(function(element) { command += '</' + element + '>' });
-      command += '</YAMAHA_AV>';
-
-      // Request the data using yamaha get
-      if (node.device.debug) {
-        node.log('sending command:' + command);
-      }
-      node.yamaha.SendXMLToReceiver(command).then(function(response){
-        xml2js.Parser({ explicitArray: false }).parseString(response, function (err, result) {
-          if (err) {
-            node.error("Failed to parse the response with error: " + err);
-            return;
-          }
-
-          // Remove reference node path from payload
-          var payload = result['YAMAHA_AV'];
-          elements.reverse().forEach(function(element) { payload = payload[element]; });
-
-          // Let's flow...
-          msg.payload = payload;
+        // Get data from the device.
+        node.deviceNode.sendGetCommand(topic).then(function(value) {
+          msg.payload = value;
           node.send(msg);
+        }).catch(function(error) {
+          node.error("Failed to request data from AVR with error: " + error);
         });
-      })
-      .catch(function(error){
-        node.error("Failed to request data from AVR with error: " + error);
+
       });
 
-      return;
-    });
+    } else {
+      this.error(RED._("avr-yamaha.errors.missing-config"));
+    }
   }
   RED.nodes.registerType("AVR-Yamaha-get", AvrYamahaNodeGet);
 
@@ -376,71 +438,45 @@ module.exports = function(RED) {
 		RED.nodes.createNode(this, config);
 
 		// Save settings in local node
-		this.device = RED.nodes.getNode(config.device);
+    this.device = config.device;
+    this.deviceNode = RED.nodes.getNode(this.device);
 		this.name = config.name;
     this.topic = config.topic;
     this.payload = config.payload;
-		this.yamaha = new YamahaAPI(this.device.address);
+
     var node = this;
+    if (this.deviceNode) {
 
-		// Input handler, called on incoming flow
-		this.on('input', function(msg) {
+  		// Input handler, called on incoming flow
+  		this.on('input', function(msg) {
 
-      // If no topic is given in the config, then we us the topic in the msg.
-      var topic = (node.topic) ? node.topic : msg.topic;
-      if (!topic) {
-        node.error('No topic given. Specify either in the config or via msg.topic!');
-        return;
-      }
+        // If no topic is given in the config, then we us the topic in the msg.
+        var topic = (node.topic) ? node.topic : msg.topic;
+        if (!topic) {
+          node.error('No topic given. Specify either in the config or via msg.topic!');
+          return;
+        }
+        
+        // If no payload is given in the config, then we us the payload in the msg.
+        var payload = (node.payload) ? node.payload : msg.payload;
+        if (payload === null || payload === undefined) {
+          node.error('invalid payload: ' + payload.toString());
+          return;
+        }
 
-      // If no payload is given in the config, then we us the payload in the msg.
-      var payload = (node.payload) ? node.payload : msg.payload;
-      if (payload === null || payload === undefined) {
-        node.error('invalid payload: ' + payload.toString());
-        return;
-      }
-
-      // Build command string from topic
-      var command = '<YAMAHA_AV cmd="PUT">';
-      var elements = topic.split('.');
-      elements.forEach(function(element) { command += '<' + element + '>' });
-      command += payload.toString().trim();
-      elements.reverse().forEach(function(element) { command += '</' + element + '>' });
-      command += '</YAMAHA_AV>';
-
-      // Write the data using yamaha put
-      if (node.device.debug) {
-        node.log('sending command:' + command);
-      }
-      node.yamaha.SendXMLToReceiver(command).then(function(response){
-        xml2js.Parser({ explicitArray: false }).parseString(response, function (err, result) {
-          if (err) {
-            node.log('Failed to parse the response with error: ' + err);
-            return;
-          }
-
-          // Traverse the response data and check for valid resonse.
-          var payload = result['YAMAHA_AV'];
-          elements.reverse().forEach(function(element) {
-            if (!payload.hasOwnProperty(element)) {
-              node.error('Received unexpected response data: ' + result);
-            }
-            payload = payload[element];
-          });
-
-          // Continue the flow with successfull write...
+        // Put data to the device.
+        node.deviceNode.sendPutCommand(topic, payload).then(function(value) {
+          // Continue the flow when data has been put...
           node.send(msg);
+        }).catch(function(error) {
+          node.error("Failed to put data to AVR with error: " + error);
         });
-      })
-      .catch(function(error){
-        node.error('Failed to write data from AVR with error: ' + error);
-      });
-		});
 
-		// Node gets closed, tidy up any state
-		this.on('close', function() {
-			node.yamaha = null;
-		});
+  		});
+
+    } else {
+      this.error(RED._("avr-yamaha.errors.missing-config"));
+    }
 	}
 	RED.nodes.registerType("AVR-Yamaha-put", AvrYamahaNodePut);
 
